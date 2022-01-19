6e52bf59fbd9cc5f76467916703db3da9ddbe4bc
LLAMA <46287788+DaLLamaKing@users.noreply.github.com>
2022-01-18 20:16:33 -0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -9,4 +9,5 @@ query = "serdar"
 htmldata = getdata("https://www.google.com/search?q=" + query + "&tbm=isch") 
 soup = BeautifulSoup(htmldata, 'html.parser') 
 for item in soup.find_all('img'):
-    print(item['src'])
+    if(item.parent.class == "bRMDJf islir"):
+       print(item['src'])
